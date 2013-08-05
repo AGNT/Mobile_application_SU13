2245deac446bb251bc28f4cbd806d6634a9f814c
Arpith <arpith@live.com>
2013-08-05 09:19:05 -0700

    added menu activity

--- android/Locate/gen/com/agnt/locate/R.java
+++ android/Locate/gen/com/agnt/locate/R.java
@@ -182,19 +182,27 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020018;
     }
     public static final class id {
-        public static final int action_settings=0x7f050006;
+        public static final int action_settings=0x7f05000c;
+        public static final int button1=0x7f050007;
+        public static final int button2=0x7f050008;
+        public static final int button3=0x7f050009;
         public static final int hybrid=0x7f050004;
+        public static final int imageView1=0x7f05000b;
         public static final int map=0x7f050005;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int satellite=0x7f050002;
         public static final int terrain=0x7f050003;
+        public static final int textView1=0x7f050006;
+        public static final int textView2=0x7f05000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_menu=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f090000;
+        public static final int menu=0x7f090001;
     }
     public static final class string {
         public static final int action_settings=0x7f060016;
@@ -278,6 +286,7 @@ containing a value of this type.
          */
         public static final int common_signin_button_text_long=0x7f06000e;
         public static final int hello_world=0x7f060017;
+        public static final int title_activity_menu=0x7f060018;
     }
     public static final class style {
         /** 
---
+++ android/Locate/src/com/agnt/locate/Menu.java
@@ -0,0 +1,13 @@
+package com.agnt.locate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Menu extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu);
+	}
+}
