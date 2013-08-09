23309505dc0ca45d75acb7e9fe6c6a495c3e4fc7
Grégoire <gregoire.decorvet@heig-vd.ch>
2013-08-09 17:27:07 +0200

    Fix database path
    Now "team2" instead of "__default" :D

--- server/Project Netbeans/team2/build/generated-sources/ap-source-output/target/Target_.java
+++ server/Project Netbeans/team2/build/generated-sources/ap-source-output/target/Target_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-08-08T16:32:47")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-08-09T17:22:00")
 @StaticMetamodel(Target.class)
 public class Target_ { 
 
